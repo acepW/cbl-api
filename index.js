ad70629409b8a6085b3ce982ca69a53c143ff8ff
acepwahyu <acepwahyu001@gmail.com>
2024-06-12 16:00:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const mspointm1 = require("./model/masterData/mtc/preventive/inspenctionPoinPm1M
 const mskpi = require("./model/masterData/mtc/kpi/masterKpiModel");
 const kpiActual = require("./model/mtc/kpi/kpiActual");
 const kpiTicket = require("./model/mtc/kpi/kpiTicket");
-
 const tcpm1 = require("./model/mtc/preventive/pm1/ticketPm1");
 const pointpm1 = require("./model/mtc/preventive/pm1/pointPm1");
 const taskpm1 = require("./model/mtc/preventive/pm1/taskPm1");
